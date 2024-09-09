c20e8faf9ba4dc298193231931b0134097ddfe0f
Ana Paula <ap.almeidapm@gmail.com>
2024-09-09 15:12:13 +0100

    criacao de paginas e arquivos essenciais para o projeto

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 function mostrarInformacoes(){
-    var inNome = document.getElementById("inNome").value;
+    var inNome = $("#inNome").val();
     var inDataNascimento = document.getElementById("inDataNascimento").value;
     var inEndereco = document.getElementById("inEndereco").value;
     var inTelefone = document.getElementById("inTelefone").value;
     var inEmail = document.getElementById("inEmail").value;
     var inGenero = document.querySelector('input[name="genero"]:checked').value;
     
-    outNome.textContent = "Nome: " + inNome;
+    //outNome.textContent = "Nome: " + inNome;
+    $("#outNome").text("Nome: " + inNome);
     outDataNasc.textContent = "Data de Nascimento: " + inDataNascimento;
     outGenero.textContent = "Gênero: " + inGenero;
     outEndereco.textContent = "Endereço: " + inEndereco;
     outTelefone.textContent = "Telefone: " + inTelefone;
     outEmail.textContent = "E-mail: " + inEmail;
 }
+
+var form = document.getElementById("form");
+
+function cadastrar(e){
+    e.preventDefault();
+    localStorage.setItem("nome",inNome.value);
+    localStorage.setItem("data de nascimento",inDataNascimento.value);
+    localStorage.setItem("endereço",inEndereco.value);
+    localStorage.setItem("telefone",inTelefone.value);
+    localStorage.setItem("email",inEmail.value);
+    localStorage.setItem("gênero",inGenero.value);
+}
+
+form.addEventListener("submit", cadastrar);
