bcbe9a0a7b14c12f8670a8646eaea8cbe288d802
Ana Paula <ap.almeidapm@gmail.com>
2024-11-05 17:33:17 +0000

    add a pagina cadastrar-cliente com a conecao com o banco de dados, addo componente full calendar

--- cadastrar-cliente.php
+++ cadastrar-cliente.php
@@ -3,11 +3,19 @@
     require("./conexao.php");
 
     $nome = filter_input(INPUT_POST, 'nomeCliente');
+    $sexo = filter_input(INPUT_POST, 'sexoCliente');
+    $dataNasc = filter_input(INPUT_POST, 'dataNascimento');
+    $convenio = filter_input(INPUT_POST, 'convenioCliente');
+    $endereco = filter_input(INPUT_POST, 'inputEndereco');
+    $cidade = filter_input(INPUT_POST, 'inputCidade');
     $email = filter_input(INPUT_POST, 'emailCliente', FILTER_VALIDATE_EMAIL);
+    $telefone = filter_input(INPUT_POST, 'inputTelefone');
+    $cpf = filter_input(INPUT_POST, 'inputCPF');
+    $observacoes = filter_input(INPUT_POST, 'textarea');
 
-    if($nome && $email){
+    if($nome && $email && $sexo && $dataNasc && $convenio && $endereco && $cidade && $telefone && $cpf && $observacoes){
         // SQL injection
-        $pdo -> query("INSERT INTO clientes(nome,email) VALUES ('$nome' ,'$email')");
+        $pdo -> query("INSERT INTO clientes(nome,email,sexo,dataNascimento,convenio,endereco,cidade,telefone,cpf,obs) VALUES ('$nome' ,'$email', '$sexo', '$dataNasc', '$convenio', '$endereco', '$cidade', '$telefone', '$cpf', '$observacoes')");
     }else{
         header('location:./novo-cliente.html');
         exit;
--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Agendamento</title>
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/styleCalendar.css">
     <!--links bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!--Link da biblioteca do calendario-->
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -45,5 +59,6 @@
 <div class="m-4">
     <h2>Bem-vindo(a) ao seu sistema de agendamento!!</h2>
 </div>
+<div id="calendar"></div>
 </body>
 </html>
